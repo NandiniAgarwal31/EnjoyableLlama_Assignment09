176516429035e7abf34de8223015a6947e997797
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-11-05 13:09:52 -0500

    added databse of grocery store simulator

--- EnjoyableLlama_Assignment09/sqlPackage/sql.py
+++ EnjoyableLlama_Assignment09/sqlPackage/sql.py
@@ -10,7 +10,7 @@ def connect_to_database():
     try:
         conn = pyodbc.connect('Driver={SQL Server};'
                               'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
-                              'Database=IS4010;'
+                              'Database=GroceryStoreSimulator;'
                               'uid=IS4010Login;'
                               'pwd=P@ssword2;')
         cursor = conn.cursor()
