dec287d49637707286ce7a18334c3defaa23bc06
Nandini Agarwal <nandiniagarwal1710@gmail.com>
2024-11-05 13:38:04 -0500

    added code to main.py

--- EnjoyableLlama_Assignment09/mainPackage/main.py
+++ EnjoyableLlama_Assignment09/mainPackage/main.py
@@ -1,27 +1,44 @@
 # main.py
+from sqlPackage.sql import *
+import random
 
-from itertools import product
-import re
-import pyodbc
-from sqlPackage.sql import * 
-try:
-    conn = connect_to_database()  # Add parentheses to call the function
-    cursor = conn.cursor()
-   
-except Exception as e:
-    print("Error accessing database")
-    print(e)
-    exit()  # Give up
+if __name__ == "__main__":
+    # Step 1: Connect to the database
+    conn = connect_to_database()
+    if conn is None:
+        print("Failed to connect to the database.")
+    else:
+        # Step 2: Fetch product data
+        product_data = fetch_product_data(conn)
+        if not product_data:
+            print("No product data found.")
+            conn.close()
+        else:
+            # Step 3: Randomly select one row from product data
+            selected_product = random.choice(product_data)
+            product_id, upc, description, manufacturer_id, brand_id = selected_product
 
-# If I got this far, I have a cursor object
-product_ID = input("Enter a Big 12 school: ")
-query_string = "SELECT * FROM tProduct WHERE NameID  = '" + product_ID +"'"
-print(query_string)
+            # Step 4: Fetch manufacturer name
+            manufacturer_name = fetch_manufacturer_name(conn, manufacturer_id)
+            if not manufacturer_name:
+                print("Manufacturer not found.")
+                conn.close()
+            else:
+                # Step 5: Fetch brand name
+                brand_name = fetch_brand_name(conn, brand_id)
+                if not brand_name:
+                    print("Brand not found.")
+                    conn.close()
+                else:
+                    # Step 6: Fetch number of items sold
+                    items_sold = fetch_items_sold(conn, product_id)
 
-# submit the qury string to our databse server and srore the results in a varible
-results = cursor.execute(query_string)
-for row in results.fetchall(): 
-    print(row[4]) # we tried to print results and we tried to print.description 
+                    # Step 7: Construct output sentence
+                    output_sentence = (
+                        f"The product '{description}', manufactured by '{manufacturer_name}' under the brand '{brand_name}', "
+                        f"has sold a total of {items_sold} items."
+                    )
+                    print(output_sentence)
 
-
-conn.close() # close the connection to the DB server
+                    # Close the database connection
+                    conn.close()
